0fe38cc4eb2cb08130bfdc22241839eec2d9c671
Nayan Das <nayan010das@gmail.com>
2025-06-06 21:39:28 +0530

    depolyment fixes

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io(import.meta.env.VITE_BACKEND_URL );
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
+    transports: ['websocket'], // force WebSocket (optional)
+});
 export default socket;
